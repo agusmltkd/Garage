cada081d3dd2751c23b4dbdae45fd00847c5ceaf
agusmltkd <67504046+agusmltkd@users.noreply.github.com>
2026-08-26 15:06:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'garaje-v5';
+const CACHE = 'garaje-v6';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
